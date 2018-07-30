20a8388e142e25d837e043ee729dee230a009e69
Alfin Jose <alfinj10@gmail.com>
2018-07-30 14:07:30 +0100

    StandardGradeBook class created

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+
+        public StandardGradeBook(string name) :base(name)
+        {
+
+                
+        }
+
+
+    }
+}
